056fd765a3787589c9243710a857deea7e942e68
Weihan Li <weihanli@outlook.com>
2024-11-30 00:10:08 +0800

    Update build.cs
    use SearchOption.AllDirectories

--- build.cs
+++ build.cs
@@ -1,22 +1,9 @@
 var root = Directory.GetCurrentDirectory();
-foreach (var scripts in GetScripts(root))
+foreach (var scripts in Directory.GetFiles(root, "*.cs", SearchOption.AllDirectories))
 {
     foreach (var script in scripts)
     {
         Console.WriteLine($"Compile script {script} test");
         CommandExecutor.ExecuteCommandAndOutput($"dotnet-exec {script} --dry-run").EnsureSuccessExitCode();
     }
-}
-
-IEnumerable<string[]> GetScripts(string root)
-{
-	foreach (var dir in Directory.GetDirectories(root))
-	{
-		yield return Directory.GetFiles(dir, "*.cs");
-		
-		foreach (var scripts in GetScripts(dir))
-		{
-			yield return scripts;
-		}
-	}
 }
