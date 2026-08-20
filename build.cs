2b95e555f02793dd67e1f1390908d3e4b01d7479
Weihan Li <weihanli@outlook.com>
2026-08-20 10:39:25 +0800

    Update build.cs
    enable source generator for build

--- build.cs
+++ build.cs
@@ -1,6 +1,6 @@
 foreach (var script in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.cs", SearchOption.AllDirectories))
 {
     Console.WriteLine($"Compile script {script} test begin");
-    CommandExecutor.ExecuteCommandAndOutput($"dotnet-exec {script} --dry-run").EnsureSuccessExitCode();
+    CommandExecutor.ExecuteCommandAndOutput($"dotnet-exec {script} --generator --dry-run").EnsureSuccessExitCode();
     Console.WriteLine($"Compile script {script} test pass");
 }
